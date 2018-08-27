eee11c13b24d0d2979eb5e3369b109bed1e5d8ac
chuangao <chuan.gao.cornell@gmail.com>
2018-08-27 00:29:32 -0400

    simplifiled install.sh, addressed the issue that lib directory was not created correctly

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 
+
+mkdir -p ./include
+mkdir -p ./lib
+
 tar -xzvf gsl-2.2.tar.gz
 cd gsl-2.2
 
@@ -8,16 +12,16 @@ make install
 cp -r include/gsl ../include/
 cp -r lib/* ../lib/
 
-cd ../
+#cd ../
 
-mkdir -p ./include/R
-echo "loc = R.home('include');write.table(loc,'loc_include.txt',row.names=F,col.names=F,sep=',',quote=F)"  | R --vanilla
-locinclude=$(cat loc_include.txt)
-cp -r $locinclude/* ./include/R/
+#mkdir -p ./include/R
+#echo "loc = R.home('include');write.table(loc,'loc_include.txt',row.names=F,col.names=F,sep=',',quote=F)"  | R --vanilla
+#locinclude=$(cat loc_include.txt)
+#cp -r $locinclude/* ./include/R/
 
-mkdir -p ./lib
-echo "loc = R.home('lib');write.table(loc,'loc_lib.txt',row.names=F,col.names=F,sep=',',quote=F)"  | R --vanilla
-loclib=$(cat loc_lib.txt)
-cp -r $loclib/* ./lib/
+#mkdir -p ./lib
+#echo "loc = R.home('lib');write.table(loc,'loc_lib.txt',row.names=F,col.names=F,sep=',',quote=F)"  | R --vanilla
+#loclib=$(cat loc_lib.txt)
+#cp -r $loclib/* ./lib/
 
-ls
+#ls
